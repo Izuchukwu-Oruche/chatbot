3654cdf2f49a44ca39f91b62ef245228399a13e3
Izuchukwu Oruche <izuchukwuoruche@gmail.com>
2025-10-27 12:53:05 +0100

    Trying to save the system prompt in an s3 bucket; hence had to adjust two file

--- bedrock_call.py
+++ bedrock_call.py
@@ -1,12 +1,12 @@
 from config import brt
-from config import MODEL_ID, SYSTEM_PROMPT
+from config import MODEL_ID, SYSTEM_PROMPT, body
 import re, json
 
 
 def call_bedrock(user_text: str) -> dict:
     resp = brt.converse(
         modelId=MODEL_ID,
-        system=[{"text": SYSTEM_PROMPT}],
+        system=[{"text": body}],
         messages=[{"role":"user","content":[{"text": user_text}]}],
         inferenceConfig={"maxTokens":800,"temperature":0.2,"topP":0.9}
     )
--- config.py
+++ config.py
@@ -7,6 +7,19 @@ WHATSAPP_TOKEN    = os.environ["WHATSAPP_TOKEN"]
 VERIFY_TOKEN      = os.environ["VERIFY_TOKEN"]
 BEDROCK_REGION    = os.getenv("BEDROCK_REGION","us-east-1")
 MODEL_ID          = os.getenv("BEDROCK_MODEL_ID","us.anthropic.claude-3-5-haiku-20241022-v1:0")
-SYSTEM_PROMPT     = os.environ["SYSTEM_PROMPT"]
+# SYSTEM_PROMPT     = os.environ["SYSTEM_PROMPT"]
+
+
+s3 = boto3.client("s3")
+CFG = None
+
+if CFG is None:
+    params = {
+        "Bucket": os.environ["CFG_BUCKET"],
+        "Key": os.environ["CFG_KEY"],
+    }
+    obj = s3.get_object(**params)
+    body = obj["Body"].read().decode("utf-8")
+    
 
 brt = boto3.client("bedrock-runtime", region_name=BEDROCK_REGION)
